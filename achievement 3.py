45d6383019c06a678dabd54bc93baef2c7429974
kasinathjayaprakash <115113787+kasinathjayaprakash@users.noreply.github.com>
2022-10-05 22:17:14 -0400

    Add files via upload

---
+++ achievement 3.py
@@ -0,0 +1,18 @@
+#author - Kasinath jayaprakash
+#achievement task - 3
+#this is used to find the correct username according to the conditions
+
+#asking the user for entering the username
+
+username = (input("please enter your username.\n"))
+#if the username is less than or equal to one letter
+if len(username) <=1:
+    print("sorry,username must be longer than one letter")
+#if the username is greater than or equal to 21 letters 
+if len(username) >=21:
+    print("sorry, username must not more than 20 letters in length")
+#if the username does not contain an uppercase or a number 
+if   username.isalnum() == username or username.upper() == username or username.lower()== username:
+    print("the username must contain atleast a uppercase and a numbera(0-9)" ,"re-enter the username ")
+else:
+    print("perfect username")    
